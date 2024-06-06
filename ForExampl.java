1f293834f1f7368fffedf32a0dc4cd5da95eeaa5
Urmila-Chaudhary <161577372+Urmila-Chaudhary@users.noreply.github.com>
2024-06-06 22:19:54 +0530

    Create ForExampl.java

---
+++ ForExampl.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) { 
+    int number = 13;
+​
+    // Print the multiplication table for the number 13
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(number + " x " + i + " = " + (number * i));
+    }
+  }
+}
+​
